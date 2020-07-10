0f62b50729bcb88aa471fafc846d0090140889be
EarlyWorm1 <1425114974@qq.com>
2020-07-10 21:01:52 +0800

    新增开启指纹登录的相关代码

--- app/src/main/java/dhu/cst/yinqingbo416/sports/MainActivity.java
+++ app/src/main/java/dhu/cst/yinqingbo416/sports/MainActivity.java
@@ -6,20 +6,33 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.util.Log;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.litepal.crud.DataSupport;
 import org.litepal.tablemanager.Connector;
 
+import java.security.KeyStore;
 import java.util.List;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 import dhu.cst.yinqingbo416.sports.Entry.ActivityInfo;
 import dhu.cst.yinqingbo416.sports.Entry.News;
+import dhu.cst.yinqingbo416.sports.Fragment.FingerprintDialogFragment;
 import dhu.cst.yinqingbo416.sports.Fragment.FragmentActivities;
 import dhu.cst.yinqingbo416.sports.Fragment.FragmentHistory;
 import dhu.cst.yinqingbo416.sports.Fragment.FragmentMyself;
@@ -34,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     private FragmentHistory fh;
     private FragmentNews fn;
     private FragmentMyself fm;
+    private static final String DEFAULT_KEY_NAME = "default_key";
+    private KeyStore keyStore;
+    private FingerprintDialogFragment fingerprintDialogFragment;
     private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -167,4 +183,81 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //开启指纹识别
+    public void startFingerprint(){
+        if (supportFingerprint()) {
+            initKey();
+            initCipher();
+        }
+    }
+    //指纹识别成功
+    public void onAuthenticated(){
+        fm.fingerprintSwitch();
+        //fingerprintDialogFragment.onPause();
+        fingerprintDialogFragment.dismiss();
+    }
+    //检查是否支持指纹识别
+    public boolean supportFingerprint(){
+        if (Build.VERSION.SDK_INT < 23) {
+            Toast.makeText(this, "您的系统版本过低，不支持指纹功能", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            KeyguardManager keyguardManager = getSystemService(KeyguardManager.class);
+            FingerprintManager fingerprintManager = getSystemService(FingerprintManager.class);
+            if (!fingerprintManager.isHardwareDetected()) {
+                Toast.makeText(this, "您的手机不支持指纹功能", Toast.LENGTH_SHORT).show();
+                return false;
+            } else if (!keyguardManager.isKeyguardSecure()) {
+                Toast.makeText(this, "您还未设置锁屏，请先设置锁屏并添加一个指纹", Toast.LENGTH_SHORT).show();
+                return false;
+            } else if (!fingerprintManager.hasEnrolledFingerprints()) {
+                Toast.makeText(this, "您至少需要在系统设置中添加一个指纹", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        return true;
+    }
+    //初始化对称密钥
+    @TargetApi(23)
+    private void initKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+            KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(DEFAULT_KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7);
+            keyGenerator.init(builder.build());
+            keyGenerator.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //初始化Cipher
+    @TargetApi(23)
+    private void initCipher() {
+        try {
+            SecretKey key = (SecretKey) keyStore.getKey(DEFAULT_KEY_NAME, null);
+            Cipher cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                    + KeyProperties.BLOCK_MODE_CBC + "/"
+                    + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            showFingerPrintDialog(cipher);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //显示指纹识别UI
+    private void showFingerPrintDialog(Cipher cipher) {
+        fingerprintDialogFragment = new FingerprintDialogFragment();
+        fingerprintDialogFragment.setCipher(cipher);
+        fingerprintDialogFragment.show(getFragmentManager(), "fingerprint");
+    }
+    //设置指纹开启状态
+    public void setFingerprintSwitch(boolean fingerprintSwitch){
+        fm.setFingerprintSwitch(fingerprintSwitch);
+    }
 }
